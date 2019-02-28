28d90ada69e9391c6a74b7e9bd1813cbe41de5e0
talios0 <charlesreverand.01@gmail.com>
2019-02-28 15:36:17 -0500

    UI Management and State Holder
    UI Manager will automatically update UI in correspondence with updates to the State. State can be modified by calling State.state = GameStates. ... State is a static class, so an instance of it is not needed, however, changes may be needed once the multiplayer components are implemented.

---
+++ Orion/Assets/_Scripts/Managers/State.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Keeper of the local game's state.
+/// Used to determine side-specific functions and the UI.
+/// </summary>
+
+public static class State {
+    public static GameStates state;
+}
+
+public enum GameStates {
+    NONE,
+    Stealth,
+    Seeker,
+    Death,
+    Pause
+}
---
+++ Orion/Assets/_Scripts/Managers/UI Manager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UI manager
+/// </summary>
+
+public class UIManager : MonoBehaviour
+{
+    // <-- UIs --> //
+    [Header("State UIs")]
+    public GameObject None;
+    public GameObject Stealth;
+    public GameObject Seeker;
+    public GameObject Death;
+    public GameObject Pause;
+
+    // <-- Management --> //
+    private GameObject _loadedUI;
+    private GameStates _loadedState;
+
+    // <-- Main Stack --> // 
+    public void UpdateUI() {
+        if (_loadedState != State.state)
+        {
+            if (_loadedUI == null)
+                DestroyUI();
+
+            CreateUI();
+        }
+    }
+
+    private void CreateUI() {
+        switch (State.state) {
+            case GameStates.NONE:
+                break;
+            case GameStates.Stealth:
+                break;
+            case GameStates.Seeker:
+                break;
+            case GameStates.Death:
+                break;
+            case GameStates.Pause:
+                break;
+        }
+
+        // Create UI
+    }
+
+    private void DestroyUI() {
+        // TEMP: Change to animations
+        Destroy(_loadedUI);
+        _loadedUI = null;
+    }
+
+    // <-- Getters/Setters --> //
+    public GameObject GetUI() {
+        return _loadedUI;
+    }
+
+}
