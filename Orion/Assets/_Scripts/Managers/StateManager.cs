7f8f1b6b03757711458d571a4fd3eb94a1dae512
talios0 <charlesreverand.01@gmail.com>
2019-03-14 15:53:46 -0400

    Basic StateManager

---
+++ Orion/Assets/_Scripts/Managers/StateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles the changing of States of the main player
+/// </summary>
+
+public class StateManager : MonoBehaviour
+{
+    // <-- Managers --> //
+    public UIManager UI;
+
+    public void ChangeState(GameStates _state) {
+        State.state = _state;
+        UI.UpdateUI();
+    }
+}
