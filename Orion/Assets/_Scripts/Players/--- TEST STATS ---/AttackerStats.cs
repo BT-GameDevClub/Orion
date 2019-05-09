f1bcdd0b83ee183d2b72f36e076225382dce2900
talios0 <charlesreverand.01@gmail.com>
2019-05-09 15:49:47 -0400

    Stats
    Attackers and Defender stats are separated. Each stores the player's statistics. This includes health and inventory.

---
+++ Orion/Assets/_Scripts/Players/--- TEST STATS ---/AttackerStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Attacker Stats", menuName = "Stats/Attacker", order = 1)]
+public class AttackerStats : ScriptableObject
+{
+    public float maxHealth;
+    public float currentHealth;
+
+    // Also store inventory, abilities, etc. here
+}
