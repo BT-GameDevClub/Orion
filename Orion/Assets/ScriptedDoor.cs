c37a07f319da1643e00076f47b211ad2b240771c
talios0 <charlesreverand.01@gmail.com>
2019-05-09 15:54:29 -0400

    Creating interactable objects

---
+++ Orion/Assets/ScriptedDoor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedDoor : MonoBehaviour // Change to universal component in the future
+{
+    public void Open() {
+    }
+}
