d6ab3ce84ac40f09f8f04957b5b3321f3764ac77
talios0 <charlesreverand.01@gmail.com>
2019-05-09 20:06:25 -0400

    Extension to Previous Commit

--- Orion/Assets/GameplayTrigger.cs
+++ Orion/Assets/GameplayTrigger.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class GameplayTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ScriptedDoor door;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter() {
+        door.Open();
     }
 }
