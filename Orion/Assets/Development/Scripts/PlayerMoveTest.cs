d1a9e538b13dad337b1ed2942525e1201ccc2070
talios0 <20057606+talios0@users.noreply.github.com>
2019-09-24 06:31:43 -0400

    Beginning Player Camera

---
+++ Orion/Assets/Development/Scripts/PlayerMoveTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveTest : MonoBehaviour
+{
+    // SCRIPT IS FOR TESTING ONLY, NOT TO BE USED FOR PLAYER MOVEMENT
+
+
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 input = GetInput();
+        rb.AddForce(input * 25);
+    }
+
+    private Vector3 GetInput() {
+        return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+}
